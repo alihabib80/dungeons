363f6c0fcfc45a41a7ea1c020d2a1b885d6af4e4
Ali Alhabeeb <alihabib80@hotmail.com>
2026-05-27 13:10:17 +0300

    comment for db connection function

--- db/db.js
+++ db/db.js
@@ -1,6 +1,7 @@
 import sqlite3 from 'sqlite3'
 import { open } from 'sqlite'
 
+// helper function to open a database connection
 export const getDB = async () => { 
     return await open({
         filename: './database.db',
